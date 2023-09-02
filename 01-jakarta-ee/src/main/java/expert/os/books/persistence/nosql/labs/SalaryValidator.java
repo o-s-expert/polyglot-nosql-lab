995572a1eee21e10c09cca4eb6427c5220adf7c8
Otavio Santana <otaviopolianasantana@gmail.com>
2023-09-02 07:44:58 +0100

    feat: create structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 01-jakarta-ee/src/main/java/expert/os/books/persistence/nosql/labs/SalaryValidator.java
@@ -0,0 +1,13 @@
+package expert.os.books.persistence.nosql.labs;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.joda.money.Money;
+
+public class SalaryValidator implements ConstraintValidator<SalaryConstraint, Money> {
+
+    @Override
+    public boolean isValid(Money value, ConstraintValidatorContext context) {
+        return value != null && !value.isNegativeOrZero();
+    }
+}
